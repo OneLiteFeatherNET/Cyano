2e88c52275a2a5f9671e70a8a37a53cfee89bcf5
theEvilReaper <steffenwx@gmail.com>
2025-06-24 10:20:32 +0200

    Add beforeEach callback

--- src/main/java/net/minestom/testing/extension/MicrotusExtension.java
+++ src/main/java/net/minestom/testing/extension/MicrotusExtension.java
@@ -14,8 +14,9 @@ import java.util.List;
  * This extension is used to resolve parameters of type {@link Env} and to intercept test method invocations.
  *
  * @since 1.5.0
+ * @version 1.1.0
  */
-public class MicrotusExtension extends TypeBasedParameterResolver<Env> implements InvocationInterceptor {
+public class MicrotusExtension extends TypeBasedParameterResolver<Env> implements InvocationInterceptor, BeforeEachCallback {
 
     /**
      * Resolves the parameter of type {@link Env}.
@@ -47,5 +48,16 @@ public class MicrotusExtension extends TypeBasedParameterResolver<Env> implement
             Env env = (Env) o;
             env.cleanup();
         });
+    }
+
+    /**
+     * This method is called before each test method execution to set up the environment.
+     * It sets the system property "minestom.viewable-packet" to "false".
+     *
+     * @param context the extension context for the test method about to be executed; never {@code null}
+     */
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        System.setProperty("minestom.viewable-packet", "false");
     }
 }
