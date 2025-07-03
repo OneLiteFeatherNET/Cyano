06cd71febe9327e148bf2b9066cd23aa29e3085c
Steffen Wonning <steffenwx@gmail.com>
2025-07-03 22:30:53 +0200

    Update Minestom to 1.21.5 (#16)
    * Update to the latest release of Minestom
    
    * Add junit to the test dependencies
    
    * Add integration the for the custom player implementation
    
    * Upgrade Minestom
    
    # Conflicts:
    #	settings.gradle.kts
    
    * Migrate to the new versioning schema from Minestom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,13 +7,17 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("libs") {
-            version("minestom", "a1d1920a04")
-            version("junit-jupiter", "5.13.2")
+            version("minestom", "2025.07.03-1.21.5")
+            version("junit", "5.13.2")
+            version("junit.platform", "1.13.1")
 
-            library("minestom","net.minestom", "minestom-snapshots").versionRef("minestom")
+            library("minestom","net.minestom", "minestom").versionRef("minestom")
 
-            library("junit-api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit-jupiter")
-            library("junit-params", "org.junit.jupiter", "junit-jupiter-params").versionRef("junit-jupiter")
+            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
+            library("junit.params", "org.junit.jupiter", "junit-jupiter-params").versionRef("junit")
+            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+            library("junit.platform.launcher", "org.junit.platform", "junit-platform-launcher").versionRef("junit.platform")
+
         }
     }
 }
