1880c4f613a62090c2bb347c45ad3363e4c259ae
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-12-16 08:33:56 +0100

    chore(build): remove double junit version definition

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,14 +9,13 @@ dependencyResolutionManagement {
         create("libs") {
             version("minestom", "2025.10.31-1.21.10")
             version("junit", "6.0.1")
-            version("junit.platform", "6.0.1")
 
             library("minestom","net.minestom", "minestom").versionRef("minestom")
 
             library("junit.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
             library("junit.params", "org.junit.jupiter", "junit-jupiter-params").versionRef("junit")
             library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
-            library("junit.platform.launcher", "org.junit.platform", "junit-platform-launcher").versionRef("junit.platform")
+            library("junit.platform.launcher", "org.junit.platform", "junit-platform-launcher").versionRef("junit")
 
         }
     }
